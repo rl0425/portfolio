8beaa615eef7fe4e5021eddefc82e3298b2f5578
박기찬 <rl0425@naver.com>
2023-03-05 04:35:50 +0900

    commit

--- src/ component/body/projects/Projects.js
+++ src/ component/body/projects/Projects.js
@@ -1,6 +1,31 @@
 import classes from "./Projects.module.css"
+import {useState} from "react";
 
 function Projects(){
+    const [cardHover, setCardHover] = useState([false, false, false, false, false, false])
+
+    const updateItem = (index, newValue) => {
+        setCardHover(prev => {
+            const newItems = [...prev]
+            newItems[index] = newValue
+            return newItems
+        })
+    }
+
+    const mouseoverGantt = () =>{ updateItem(0, true) }
+    const mouseoutGantt = () =>{ updateItem(0, false) }
+    const mouseoverFot = () =>{ updateItem(1, true) }
+    const mouseoutFot = () =>{ updateItem(1, false) }
+    const mouseoverEval = () =>{ updateItem(2, true) }
+    const mouseoutEval = () =>{ updateItem(2, false) }
+    const mouseoverSummary = () =>{ updateItem(3, true) }
+    const mouseoutSummary = () =>{ updateItem(3, false) }
+    const mouseoverEnter = () =>{ updateItem(4, true) }
+    const mouseoutEnter = () =>{ updateItem(4, false) }
+    const mouseoverNow = () =>{ updateItem(5, true) }
+    const mouseoutNow = () =>{ updateItem(5, false) }
+
+
     return (
         <div className={classes.box}>
             <div className={classes.head}>
@@ -9,25 +34,51 @@ function Projects(){
             </div>
             <div className={classes.body}>
                 <div className={classes.singleCard}>
-                    <div className={classes.ganttCard}>
-                        <div className={classes.cardBackground}>
-                            <span>Gantt 프로젝트</span>
+                    <div onMouseOver={mouseoverGantt} onMouseLeave={mouseoutGantt} className={classes.ganttCard}>
+                        <img src={"/images/projects/ganttMain.gif"} />
+                        <div className={cardHover[0] ? classes.cardBackground : `${classes.cardBackground} ${classes.noCard}`}>
+                            <span>간트차트 프로젝트</span>
                         </div>
                     </div>
                 </div>
                 <div className={classes.multiCard}>
                     <div className={classes.areaLeft}>
-                        <div className={classes.fotcard}>
-                            <div className={classes.fotcardBackground}>
+                        <div onMouseOver={mouseoverFot} onMouseLeave={mouseoutFot} className={classes.fotcard}>
+                            <img src={"/images/projects/fotscore.gif"} />
+                            <div className={cardHover[1] ? classes.fotcardBackground : `${classes.fotcardBackground} ${classes.noCard}`}>
                                 <span>FOTSCORE</span>
                             </div>
                         </div>
                     </div>
                     <div className={classes.areaRight}>
-                        <div className={classes.card}></div>
+                        <div onMouseOver={mouseoverEval} onMouseLeave={mouseoutEval} className={classes.evaluationCard}>
+                            <img src={"images/projects/evaluation.gif"}/>
+                            <div className={cardHover[2] ? classes.evaluationCardBackground : `${classes.evaluationCardBackground} ${classes.noCard}`}>
+                                <span>프로젝트 평가 기능</span>
+                            </div>
+                        </div>
+                        <div onMouseOver={mouseoverSummary} onMouseLeave={mouseoutSummary} className={classes.evaluationCard}>
+                            <img src={"images/projects/summary.gif"}/>
+                            <div className={cardHover[3] ? classes.evaluationCardBackground : `${classes.evaluationCardBackground} ${classes.noCard}`}>
+                                <span>프로젝트 정의 기능</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className={classes.singleCard}><div className={classes.card}></div></div>
+                <div className={classes.fotCardList}>
+                    <div onMouseOver={mouseoverEnter} onMouseLeave={mouseoutEnter} className={classes.fotCard}>
+                        <img src={"images/projects/kised_gantt.png"}/>
+                        <div className={cardHover[4] ? classes.fotCardBackground : `${classes.fotCardBackground} ${classes.noCard}`}>
+                            <span>엔터프라이즈 모델 구축</span>
+                        </div>
+                    </div>
+                    <div onMouseOver={mouseoverNow} onMouseLeave={mouseoutNow} className={classes.fotCard}>
+                        <img src={"images/projects/next.png"}/>
+                        <div className={cardHover[5] ? classes.fotCardBackground : `${classes.fotCardBackground} ${classes.noCard}`}>
+                            <span>진행중인 프로젝트들</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
